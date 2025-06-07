de8c4b84f0fe1d83431785c69d74a1292034078b
Edmundo Ogaz <edmundo.ogaz@gmail.com>
2025-06-07 12:35:51 -0400

    evitar conexiones abiertas con poolclass

--- database_postgres_serverless.py
+++ database_postgres_serverless.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.pool import NullPool
 
 from dotenv import load_dotenv
 import os
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL, 
+    poolclass=NullPool,  # <-- evita mantener la conexión abierta
+    pool_pre_ping=True)
 
 # Crea sesiones nuevas por request (modo sincrónico)
 SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
